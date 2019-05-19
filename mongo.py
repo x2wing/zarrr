488206df93b280011932b8a78be4bc28a649a1a7
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-05-19 16:10:52 +0500

    чтение запись данных в mongo

--- mongo.py
+++ mongo.py
@@ -1,1 +1,12 @@
-print('dfgg')
+from  zarr import MongoDBStore
+import pymongo
+
+
+
+import zarr
+store = zarr.MongoDBStore(database='geosim',  collection='Приобка')
+# z = zarr.zeros((10, 10), chunks=(5, 5), store=store, overwrite=True)
+z = zarr.open(store, mode='r')
+# z[...] = 42
+print(z[:])
+store.close()
